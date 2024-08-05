c0905b328f8ce48afe4c4c1adbd5b6212d220c6d
connorrewa <connorrewa13@hotmail.com>
2024-08-05 15:03:22 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import Player from './modules/Player';
-import GameController from './modules/GameController';
-const human = Player();
-const computer = Player();
-const game = GameController(human, computer);
-
-game.startGame();
